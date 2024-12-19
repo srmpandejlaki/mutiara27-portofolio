d5e0d625696e6b27f9b7a5510f416b4592b8ad22
Mutiara <mutiarapandejlaki27@gmail.com>
2024-12-19 18:07:45 +0700

    edit bagian about me dan box message

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
  
 module.exports = {
   entry: path.join(__dirname, 'source/script/index.js'),
@@ -19,6 +20,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'source/index.html'),
       filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, 'source/public/images'),
+          to: path.join(__dirname, 'dist'),
+        },
+      ],
     }),
   ],
 };
