b5c22cb485252878284b383b8e7cc5b1eb378827
Muti <mutiarapandejlaki47@gmail.com>
2026-02-05 20:06:40 +0800

    update navBar mobile

--- src/components/base/navBar.jsx
+++ src/components/base/navBar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavigationBar() {
+import IconLink from "./iconLink";
+
+function NavigationBar({ handleCloseNav }) {
   return (
-    <div className="navbar-list">
-      <NavLink to="/dashboard" className={({ isActive }) => (isActive ? "active" : "")}><i className="fa-solid fa-house"></i>Dashboard</NavLink>
-      <NavLink to="/about-me" className={({ isActive }) => (isActive ? "active" : "")}><i className="fa-solid fa-user"></i>About</NavLink>
-      {/* <NavLink to="/blog">Blog</NavLink> */}
+    <div className="nav-section">
+      <div className="nav-head">
+        <div className="logo">
+          <p>M<span>D</span>.io</p>
+          <p className="descLogo">Mutiara Digital Portofolio</p>
+        </div>
+        <i className="fa-solid fa-xmark closeBtn" onClick={handleCloseNav}></i>
+      </div>
+      <div className="navbar-list">
+        <NavLink to="/dashboard" className={({ isActive }) => (isActive ? "active" : "")}><i className="fa-solid fa-house"></i>Dashboard</NavLink>
+        <NavLink to="/about-me" className={({ isActive }) => (isActive ? "active" : "")}><i className="fa-solid fa-user"></i>About</NavLink>
+        {/* <NavLink to="/blog">Blog</NavLink> */}
+      </div>
+      <div className="nav-footer">
+        <IconLink />
+      </div>
     </div>
   );
 }
